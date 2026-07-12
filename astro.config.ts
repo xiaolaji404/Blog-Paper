a7f7ccab570769f29526d442f018ca23e0d8493b
xiaolaji <xiaolaji1357@gmail.com>
2026-07-12 17:00:29 +0800

    feat:添加站点语言 中文支持

--- astro.config.ts
+++ astro.config.ts
@@ -29,8 +29,8 @@ export default defineConfig({
     }),
   ],
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ["zh", "en"],
+    defaultLocale: "zh",
     routing: {
       prefixDefaultLocale: false,
     },
